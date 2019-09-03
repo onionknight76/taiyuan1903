3164ce2cf6f72ddc0fbdae295ad2a763030db511
onionknight76 <1840802136@qq.com>
2019-09-03 22:22:04 +0800

    这是第二个内容区

--- src/js/index.js
+++ src/js/index.js
@@ -54,7 +54,7 @@ $(function () {
 //轮播图
 var index = 0;
 var flag = true;
-$(".btn").eq(0).css("background","#ffffff");
+$(".btn").eq(0).css("background", "#ffffff");
 $("#banner").hover(function () {
     flag = false;
 
@@ -67,8 +67,8 @@ $(".btn").click(function () {
     index = $(this).index();
     alert(index);
     $(".img-item").eq(index).fadeIn().siblings().fadeOut();
-  
-    $(".btn").eq(index).css("background","#FFFFFF").siblings().css("background", "#666666")
+
+    $(".btn").eq(index).css("background", "#FFFFFF").siblings().css("background", "#666666")
     alert(index);
 })
 //点击右按钮
@@ -104,4 +104,43 @@ $(function () {
             $(".btn").eq(index).css("background", "#ffffff").siblings().css("background", "#666666")
         }
     }, 3000)
+})
+
+var b = 1;
+
+
+$(function () {
+    $(".flag-left").click(function () {
+        //alert("我太难啦");
+        var a = -980;
+        if (b > 2) {
+            b = 1;
+            return;
+        }
+        console.log($(".neirong2-second ul"));
+        c = a * b;
+        $(".neirong2-second ul").animate({ left: a * b }, 300);
+        b++;
+
+    })
+    $(".flag-right").click(function () {
+        var a = 0;
+        $(".neirong2-second ul").animate({ left: a }, 300);
+        a = 980;
+    })
+})
+
+$(function () {
+    setInterval(function(){
+        var myDate = new Date(); //实例一个时间对象；
+        // myDate.getFullYear();   //获取系统的年；
+        // myDate.getMonth() + 1;   //获取系统月份，由于月份是从0开始计算，所以要加1
+        // myDate.getDate(); // 获取系统日，
+      var hour =   myDate.getHours(); //获取系统时，
+      var minute = myDate.getMinutes(); //分
+      var second =  myDate.getSeconds(); //秒
+      $(".time span").eq(0).html(hour);
+      $(".time span").eq(1).html(minute);
+      $(".time span").eq(2).html(second);
+    },1000)
 })
